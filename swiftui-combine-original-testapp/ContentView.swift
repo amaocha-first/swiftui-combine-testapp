0f591b08f8029dc6e6ffaef1628ed745ff973f9f
NishizawaShota <nishizawa@emol.jp>
2019-10-11 15:28:26 +0900

    Initial Commit

---
+++ swiftui-combine-original-testapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui-combine-original-testapp
+//
+//  Created by Shota Nishizawa on 2019/10/11.
+//  Copyright © 2019 Shota Nishizawa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
